72de328bc8d3f3d23c7420d3821eecb1534c5567
yangui88168 <yangui88168@gmail.com>
2026-06-26 18:47:28 +0800

    Update next.config.js

--- clients/web/next.config.js
+++ clients/web/next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',   // ← 新增：生成纯静态文件
+}
 module.exports = nextConfig
