572f0381942762cd41b7fc2b046aa613d3ff2ea9
yangui88168 <yangui88168@gmail.com>
2026-06-29 02:14:36 +0800

    Create community.tsx

---
+++ clients/web/pages/zhihui/community.tsx
@@ -0,0 +1,10 @@
+export default function Community() {
+  return (
+    <div className="h-full flex items-center justify-center text-gray-400">
+      <div className="text-center">
+        <div className="text-6xl mb-4">🏘️</div>
+        <p className="text-lg">社区系统开发中</p>
+      </div>
+    </div>
+  );
+}
