a7cc86b03c0ace5c4f540eaad5b50e229c15ed24
yangui88168 <yangui88168@gmail.com>
2026-06-29 02:14:13 +0800

    Create space.tsx

---
+++ clients/web/pages/zhihui/space.tsx
@@ -0,0 +1,10 @@
+export default function Space() {
+  return (
+    <div className="h-full flex items-center justify-center text-gray-400">
+      <div className="text-center">
+        <div className="text-6xl mb-4">🏠</div>
+        <p className="text-lg">个人空间开发中</p>
+      </div>
+    </div>
+  );
+}
