cb3674e3df4e901dd1c3651150744fe872a84eb3
yangui88168 <yangui88168@gmail.com>
2026-06-29 01:46:33 +0800

    Create channel.tsx

---
+++ pages/zhihui/channel.tsx
@@ -0,0 +1,10 @@
+export default function Channel() {
+  return (
+    <div className="h-full flex items-center justify-center text-gray-400">
+      <div className="text-center">
+        <div className="text-6xl mb-4">📺</div>
+        <p className="text-lg">频道系统开发中</p>
+      </div>
+    </div>
+  );
+}
