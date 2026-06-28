368998aedd09c56bd7826927207f978517ead6b5
yangui88168 <yangui88168@gmail.com>
2026-06-29 00:00:10 +0800

    Update contacts.tsx

--- clients/web/pages/contacts.tsx
+++ clients/web/pages/contacts.tsx
@@ -163,7 +163,7 @@ export default function Contacts() {
   };
 
   return (
-    <div style={{ height: 'calc(100dvh - 56px)', overflowY: 'auto' }} className="bg-gray-50">
+    <div className="min-h-screen overflow-y-auto bg-gray-50" style={{ paddingBottom: '56px' }}>
       {/* 搜索栏 */}
       <div className="p-4 bg-white border-b">
         <div className="flex gap-2">
