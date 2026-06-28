dcdb181b7b95e23ac8cea9a10cdd2f2f4a3d0999
yangui88168 <yangui88168@gmail.com>
2026-06-29 01:46:58 +0800

    Create discover.tsx

---
+++ pages/zhihui/discover.tsx
@@ -0,0 +1,10 @@
+export default function Discover() {
+  return (
+    <div className="h-full flex items-center justify-center text-gray-400">
+      <div className="text-center">
+        <div className="text-6xl mb-4">🔍</div>
+        <p className="text-lg">发现页面开发中</p>
+      </div>
+    </div>
+  );
+}
